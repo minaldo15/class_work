4b57d73d98d8df5fdce92033f2643f28a1b718f2
minaldo <alsskfen1231@gmail.com>
2023-03-24 15:51:29 +0900

    0324

---
+++ 0324/2105디저트카페.py
@@ -0,0 +1,38 @@
+import sys
+sys.stdin = open('input.txt','r')
+
+dr = [(1,1),(1,-1),(-1,-1),(-1,1)]
+def bfs(i,j):
+    v = [[0]*N for _ in range(N)]
+    q = [(i,j)]
+    k = 0
+    alst = [arr[i][j]]
+    while q:
+        ci, cj = q.pop(0)
+        ni, nj = ci+dr[k][0], cj+dr[k][1]
+        if 0<=ni<N and 0<=nj<N and v[ni][nj] == 0:
+            if arr[ni][nj] not in alst:
+                q.append((ni,nj))
+                v[ni][nj] = 1
+                alst.append(arr[ni][nj])
+            else:
+                if arr[ni][nj] == alst[0] and k==3:
+                    print(alst)
+                    return len(alst)
+                else:
+                    k += 1
+                    q.append((ci, cj))
+        else:
+            k += 1
+            q.append((ci,cj))
+        if k == 4:
+            break
+
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    bfs(0,1)
+    bfs(0,2)
+    bfs(0,0)
